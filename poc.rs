a495bbe05492ed2ec83894676519f01e5a6a93e0
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2023-06-14 08:48:01 -0300

    Update poc.rs

--- poc.rs
+++ poc.rs
@@ -1,4 +1,4 @@
-use nostr::{PubKey as Event, Event , Event as Relay, Relay};
+use nostr::{PubKey as Event, Event, Event as Relay, Relay};
 use lightning::ln::PaymentHash;
 use lightning::chain;
 use lightning::chain::chaininterface::{BroadcasterInterface, FeeEstimator};
@@ -6,4 +6,69 @@ use lightning::sign::{NodeSigner, SignerProvider, EntropySource};
 use lightning::ln::channelmanager::{ChannelManager, PaymentId, Retry, RetryableSendFailure, RecipientOnionFields};
 use lightning::routing::router::{PaymentParameters, RouteParameters, Router};
 use lightning::util::logger::Logger;
+
+// Define your Nostr protocol application logic
+struct MyApp {
+    // Define your application-specific variables and state here
+}
+
+impl MyApp {
+    // Implement your application-specific functions here
+    // ...
+
+    // Handle Nostr protocol events
+    fn handle_event(&mut self, event: Event) {
+        match event {
+            Event::Event1(data) => {
+                // Handle Event1
+                // ...
+            }
+            Event::Event2(data) => {
+                // Handle Event2
+                // ...
+            }
+            // Handle other events
+            _ => {
+                // Handle unknown events or errors
+                // ...
+            }
+        }
+    }
+
+    // Send Nostr protocol relays
+    fn send_relay(&mut self, relay: Relay) {
+        // Send the relay to the Nostr protocol network
+        // ...
+    }
+}
+
+// Implement the Nostr protocol event handler
+impl nostr::EventHandler<Event, Relay> for MyApp {
+    fn handle_event(&mut self, event: Event) {
+        self.handle_event(event);
+    }
+
+    fn send_relay(&mut self, relay: Relay) {
+        self.send_relay(relay);
+    }
+}
+
+// Entry point of the application
+fn main() {
+    // Initialize the Nostr protocol
+    let mut nostr = nostr::Nostr::new(MyApp {});
+
+    // Subscribe to Nostr protocol events
+    nostr.subscribe(Event::Event1);
+    nostr.subscribe(Event::Event2);
+    // ...
+
+    // Publish your app on the Nostr protocol
+    nostr.publish();
+
+    // Start listening for Nostr protocol events and relays
+    nostr.listen();
+}
+
+
 
