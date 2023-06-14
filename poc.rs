448937ca4dc0553a60e977c8e6ddf69b82ca001a
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2023-06-14 09:24:35 -0300

    Update poc.rs

--- poc.rs
+++ poc.rs
@@ -1,11 +1,4 @@
 use rust_nostr::{PubKey as Event, Event, Event as Relay, Relay};
-use lightning::ln::PaymentHash;
-use lightning::chain;
-use lightning::chain::chaininterface::{BroadcasterInterface, FeeEstimator};
-use lightning::sign::{NodeSigner, SignerProvider, EntropySource};
-use lightning::ln::channelmanager::{ChannelManager, PaymentId, Retry, RetryableSendFailure, RecipientOnionFields};
-use lightning::routing::router::{PaymentParameters, RouteParameters, Router};
-use lightning::util::logger::Logger;
 
 // Define your Nostr protocol application logic
 struct MyApp {
